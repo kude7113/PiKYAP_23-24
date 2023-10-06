8e20126148d45fd4bbe6f41053d9231256190e81
denis.kuzmin <den2004kuz@gmail.com>
2023-10-06 18:27:22 +0300

    add comand start and dick

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,5 +1,16 @@
 import telebot
 
-botTimeWeb = telebot.TeleBot('5782807787:AAEcV-5TiSrSzY-vM5i05HAn9o7aYCwRHzc')
+bot = telebot.TeleBot('5782807787:AAEcV-5TiSrSzY-vM5i05HAn9o7aYCwRHzc')
 
-from telebot import types
+@bot.message_handler(commands = ['start'])
+def main(message):
+    bot.send_message(message.chat.id, message)
+
+@bot.message_handler(commands = ['dick'])
+def main(user):
+    if user.chat.type == 'private':
+        bot.send_message(user.chat.id, 'Этой командой можно пользоваться только в группах')
+    else:
+        bot.send_message(user.chat.id, 'у тебя большая писька')
+
+bot.polling(none_stop=True)
