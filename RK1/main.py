c9c032af8f015dd792ca0f46ee8df99da0363d2b
denis.kuzmin <den2004kuz@gmail.com>
2023-10-28 15:03:28 +0300

    change class name for my variant

--- RK1/main.py
+++ RK1/main.py
@@ -2,67 +2,66 @@
 from operator import itemgetter
 
 
-class Emp:
-    """Сотрудник"""
+class Book:
+    """Книга"""
 
-    def __init__(self, id, fio, sal, dep_id):
+    def __init__(self, id, name_b, price, Shop_id):
         self.id = id
-        self.fio = fio
-        self.sal = sal
-        self.dep_id = dep_id
+        self.name_b = name_b
+        self.price = price
+        self.Shop_id = Shop_id
 
 
-class Dep:
-    """Отдел"""
+class Shop:
+    """Книжный магазин"""
 
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
 
-class EmpDep:
+class BookShop:
     """
-    'Сотрудники отдела' для реализации
+    'Книга в книжном' для реализации
     связи многие-ко-многим
     """
 
-    def __init__(self, dep_id, emp_id):
-        self.dep_id = dep_id
-        self.emp_id = emp_id
+    def __init__(self, Shop_id, Book_id):
+        self.Shop_id = Shop_id
+        self.Book_id = Book_id
 
 
 # Отделы
-deps = [
-    Dep(1, 'отдел кадров'),
-    Dep(2, 'архивный отдел ресурсов'),
-    Dep(3, 'бухгалтерия'),
-
-    Dep(11, 'отдел (другой) кадров'),
-    Dep(22, 'архивный (другой) отдел ресурсов'),
-    Dep(33, '(другая) бухгалтерия'),
+Shops = [
+    Shop(1, 'Достаевский'),
+    Shop(2, 'Читай город'),
+    Shop(3, 'Республика'),
+    Shop(11, 'Фаланастер'),
+    Shop(22, 'Московский дом книги'),
+    Shop(33, 'Ноты'),
 ]
 
 # Сотрудники
-emps = [
-    Emp(1, 'Артамонов', 25000, 1),
-    Emp(2, 'Петров', 35000, 2),
-    Emp(3, 'Иваненко', 45000, 3),
-    Emp(4, 'Иванов', 35000, 3),
-    Emp(5, 'Иванин', 25000, 3),
+Books = [
+    Book(1, 'Герой нашего времени', 250, 1),
+    Book(2, 'Мастер и маргарита', 350, 2),
+    Book(3, 'Заводной апельсин', 450, 3),
+    Book(4, 'Три товарища', 350, 3),
+    Book(5, 'Портрет Дориана Грея', 250, 3),
 ]
 
-emps_deps = [
-    EmpDep(1, 1),
-    EmpDep(2, 2),
-    EmpDep(3, 3),
-    EmpDep(3, 4),
-    EmpDep(3, 5),
+Books_Shops = [
+    BookShop(1, 1),
+    BookShop(2, 2),
+    BookShop(3, 3),
+    BookShop(3, 4),
+    BookShop(3, 5),
 
-    EmpDep(11, 1),
-    EmpDep(22, 2),
-    EmpDep(33, 3),
-    EmpDep(33, 4),
-    EmpDep(33, 5),
+    BookShop(11, 1),
+    BookShop(22, 2),
+    BookShop(33, 3),
+    BookShop(33, 4),
+    BookShop(33, 5),
 ]
 
 
@@ -70,20 +69,20 @@ def main():
     """Основная функция"""
 
     # Соединение данных один-ко-многим
-    one_to_many = [(e.fio, e.sal, d.name)
-                   for d in deps
-                   for e in emps
-                   if e.dep_id == d.id]
+    one_to_many = [(e.name_b, e.price, d.name)
+                   for d in Shops
+                   for e in Books
+                   if e.Shop_id == d.id]
 
     # Соединение данных многие-ко-многим
-    many_to_many_temp = [(d.name, ed.dep_id, ed.emp_id)
-                         for d in deps
-                         for ed in emps_deps
-                         if d.id == ed.dep_id]
+    many_to_many_tBook = [(d.name, ed.Shop_id, ed.Book_id)
+                         for d in Shops
+                         for ed in Books_Shops
+                         if d.id == ed.Shop_id]
 
-    many_to_many = [(e.fio, e.sal, dep_name)
-                    for dep_name, dep_id, emp_id in many_to_many_temp
-                    for e in emps if e.id == emp_id]
+    many_to_many = [(e.name_b, e.price, Shop_name)
+                    for Shop_name, Shop_id, Book_id in many_to_many_tBook
+                    for e in Books if e.id == Book_id]
 
     print('Задание А1')
     res_11 = sorted(one_to_many, key=itemgetter(2))
@@ -92,16 +91,16 @@ def main():
     print('\nЗадание А2')
     res_12_unsorted = []
     # Перебираем все отделы
-    for d in deps:
+    for d in Shops:
         # Список сотрудников отдела
-        d_emps = list(filter(lambda i: i[2] == d.name, one_to_many))
+        d_Books = list(filter(lambda i: i[2] == d.name, one_to_many))
         # Если отдел не пустой
-        if len(d_emps) > 0:
+        if len(d_Books) > 0:
             # Зарплаты сотрудников отдела
-            d_sals = [sal for _, sal, _ in d_emps]
+            d_prices = [price for _, price, _ in d_Books]
             # Суммарная зарплата сотрудников отдела
-            d_sals_sum = sum(d_sals)
-            res_12_unsorted.append((d.name, d_sals_sum))
+            d_prices_sum = sum(d_prices)
+            res_12_unsorted.append((d.name, d_prices_sum))
 
     # Сортировка по суммарной зарплате
     res_12 = sorted(res_12_unsorted, key=itemgetter(1), reverse=True)
@@ -110,15 +109,15 @@ def main():
     print('\nЗадание А3')
     res_13 = {}
     # Перебираем все отделы
-    for d in deps:
+    for d in Shops:
         if 'отдел' in d.name:
             # Список сотрудников отдела
-            d_emps = list(filter(lambda i: i[2] == d.name, many_to_many))
+            d_Books = list(filter(lambda i: i[2] == d.name, many_to_many))
             # Только ФИО сотрудников
-            d_emps_names = [x for x, _, _ in d_emps]
+            d_Books_names = [x for x, _, _ in d_Books]
             # Добавляем результат в словарь
             # ключ - отдел, значение - список фамилий
-            res_13[d.name] = d_emps_names
+            res_13[d.name] = d_Books_names
 
     print(res_13)
 
