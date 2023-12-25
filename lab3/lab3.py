3d125fc99e8280a3d6b60a13decfc9c83125f3b5
denis.kuzmin <den2004kuz@gmail.com>
2023-12-25 14:17:36 +0300

    otchet

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -8,9 +8,9 @@ from io import BytesIO
 bot = telebot.TeleBot('5782807787:AAEcV-5TiSrSzY-vM5i05HAn9o7aYCwRHzc')
 
 
-conn = sqlite3.connect('piski.sql')
+conn = sqlite3.connect('morkov.sql')
 cur = conn.cursor()
-cur.execute('CREATE TABLE IF NOT EXISTS piski (id int auto_increment pimary key,id_chat int, id_user int, piska int, name varchar(50),dat int)')
+cur.execute('CREATE TABLE IF NOT EXISTS morkov (id int auto_increment pimary key,id_chat int, id_user int, piska int, name varchar(50),dat int)')
 conn.commit()
 cur.close()
 conn.close()
@@ -19,9 +19,9 @@ conn.close()
 def main(message):
     bot.send_message(message.chat.id, message)
 
-@bot.message_handler(commands = ['pisun'])
+@bot.message_handler(commands = ['morkov'])
 def main(message):
-    conn = sqlite3.connect('piski.sql')
+    conn = sqlite3.connect('morkov.sql')
     cur = conn.cursor()
     if message.chat.type == 'group' or message.chat.type == 'supergroup':
         chat_id = message.chat.id
@@ -29,32 +29,32 @@ def main(message):
         user_name = message.from_user.username
         piska = 0
         time = message.date
-        cur.execute('SELECT * FROM piski')
-        piski = cur.fetchall()
+        cur.execute('SELECT * FROM morkov')
+        morkov = cur.fetchall()
         i = 0
-        for el in piski:
+        for el in morkov:
             if chat_id == el[1] and user_id == el[2]:
                 i += 1
         if i == 0:
-            cur.execute("INSERT INTO piski (id_chat, id_user, piska, name, dat) VALUES ('%d', '%d','%d', '%s', '%d')" % (chat_id, user_id, piska, user_name, time))
+            cur.execute("INSERT INTO morkov (id_chat, id_user, piska, name, dat) VALUES ('%d', '%d','%d', '%s', '%d')" % (chat_id, user_id, piska, user_name, time))
 
-        cur.execute("SELECT * FROM piski WHERE id_chat == '%d' AND id_user == '%d'" % (chat_id, user_id))
+        cur.execute("SELECT * FROM morkov WHERE id_chat == '%d' AND id_user == '%d'" % (chat_id, user_id))
         user_info = cur.fetchone()
         int_dat = user_info[5]
-        now_pisun = user_info[3]
+        now_morkov = user_info[3]
 
         if time - int_dat > 86400 or i == 0:
             rand = random.randint(-5, 10)
-            now_pisun += rand
-            cur.execute("UPDATE piski SET piska = piska + '%d' WHERE id_chat == '%d' AND id_user == '%d'" % (
+            now_morkov += rand
+            cur.execute("UPDATE morkov SET piska = piska + '%d' WHERE id_chat == '%d' AND id_user == '%d'" % (
             rand, chat_id, user_id))
             cur.execute(
-                "UPDATE piski SET dat = '%d' WHERE id_chat == '%d' AND id_user == '%d'" % (time, chat_id, user_id))
+                "UPDATE morkov SET dat = '%d' WHERE id_chat == '%d' AND id_user == '%d'" % (time, chat_id, user_id))
             q = ''
             if rand > 0:
-                q = user_name + ' ваша писька выросла на ' + str(rand) + ' см' + '\n' + 'Теперь ваш писюн: ' + str(now_pisun)
+                q = user_name + ' ваша морковка выросла на ' + str(rand) + ' см' + '\n' + 'Теперь ваш морковка: ' + str(now_morkov)
             else:
-                q = user_name + ' ваша писька сократилась на ' + str(abs(rand)) + ' см' + '\n' + 'Теперь ваш писюн: ' + str(now_pisun)
+                q = user_name + ' ваша морковка сократилась на ' + str(abs(rand)) + ' см' + '\n' + 'Теперь ваш морковка: ' + str(now_morkov)
             bot.send_message(message.chat.id, q)
         else:
             bot.send_message(message.chat.id, 'Этой командой можно пользоваться раз в 24 часа!')
@@ -66,15 +66,15 @@ def main(message):
         bot.send_message(message.chat.id, 'Этой командой можно пользоваться только в группах')
 
 
-@bot.message_handler(commands = ['top_pisun'])
+@bot.message_handler(commands = ['top_morkov'])
 def main(message):
     chat_id = message.chat.id
-    conn = sqlite3.connect('piski.sql')
+    conn = sqlite3.connect('morkov.sql')
     cur = conn.cursor()
-    cur.execute("SELECT * FROM piski WHERE id_chat == '%d'" % (chat_id))
-    piski = cur.fetchall()
+    cur.execute("SELECT * FROM morkov WHERE id_chat == '%d'" % (chat_id))
+    morkov = cur.fetchall()
     top = []
-    for el in piski:
+    for el in morkov:
         top.append([el[3], el[4]])
     top.sort(reverse = True)
     p = ''
@@ -82,17 +82,17 @@ def main(message):
         p += str(i + 1) + ') ' +str(top[i][1]) + ' - ' + str(top[i][0]) + 'см' + '\n'
     bot.send_message(message.chat.id, p)
     if top == []:
-        bot.send_message(message.chat.id, 'У всех в этом чате письки равны 0')
+        bot.send_message(message.chat.id, 'У всех в этом чате морковки равны 0')
 
 @bot.message_handler(commands = ['diagrama'])
 def main(message):
     chat_id = message.chat.id
-    conn = sqlite3.connect('piski.sql')
+    conn = sqlite3.connect('morkov.sql')
     cur = conn.cursor()
-    cur.execute("SELECT * FROM piski WHERE id_chat == '%d'" % (chat_id))
-    piski = cur.fetchall()
+    cur.execute("SELECT * FROM morkov WHERE id_chat == '%d'" % (chat_id))
+    morkov = cur.fetchall()
     top = []
-    for el in piski:
+    for el in morkov:
         top.append([el[3], el[4]])
     top.sort(reverse = True)
     dick = []
@@ -107,7 +107,7 @@ def main(message):
 
 
     plt.pie(dick, labels=name, autopct='%1.1f%%')
-    plt.title('Письки этого чата')
+    plt.title('морковки этого чата')
     plt.legend()
 
     # Сохраняем диаграмму во временный файл
@@ -120,14 +120,14 @@ def main(message):
 
 @bot.message_handler(commands = ['print'])
 def main(message):
-    conn = sqlite3.connect('piski.sql')
+    conn = sqlite3.connect('morkov.sql')
     cur = conn.cursor()
 
-    cur.execute('SELECT * FROM piski')
-    piski = cur.fetchall()
+    cur.execute('SELECT * FROM morkov')
+    morkov = cur.fetchall()
 
     info = ''
-    for el in piski:
+    for el in morkov:
         info += f'chat_id: {el[1]}, user_id: {el[2]}, piska: {el[3]}, name: {el[4]}, time: {el[5]}\n'
     cur.close()
     conn.close()
